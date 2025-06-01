a8d6a387e9801edee52aeaad86c9e5ad1548398b
DalliMani <ashpikapokemani@gmail.com>
2025-06-01 11:01:27 +0530

    Improved IO and resolved warnings from previous commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import pandas as pd
+from io import StringIO
 
 def main():
     #Opening the browser
@@ -13,12 +14,20 @@ def main():
     #Setting the URL
     url = "https://josaa.admissions.nic.in/applicant/seatmatrix/openingclosingrankarchieve.aspx"
     driver.get(url)
+
     #Waiting until the user selects the options and options are loaded
-    input('Press a key and enter when details are filled AND the table is loaded!\nCheck the scrollbar of the the browser to see if the table has been loaded completely. For safety, wait 5-10 seconds depending on net speed.\n<Click-Enter>\n')
+    print('Go to Firefox and load the table and scroll down to the end of the table!')
+    print('Check the scrollbar of the the browser to see if the table has been loaded completely. For safety, wait 10-20 seconds depending on net speed.')
+    input('<Click-Enter>\n')
+
+    #Getting the table
     table = driver.find_element(By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_GridView1"]')
-    table_html = table.get_attribute('outerHTML')
+    table_html = StringIO(table.get_attribute('outerHTML'))
     frame_list =pd.read_html(table_html) #This was just observed from nature of pd.read_html
     mainframe = frame_list[0]
+
+    #Closing the HTML because we don't need it
+    table_html.close()
 
     #Coverting the OR and CR columns to integer because the fricking excel sort doesn't work properly with text
     mainframe['Opening Rank'] = pd.to_numeric(mainframe['Opening Rank'], downcast='integer', errors='coerce')
@@ -27,8 +36,8 @@ def main():
     #Keeping the filesaving convention to fixed for now (lazy)
     filename = 'JoSAA_Data_0'
     should_save_as = {
-        'Excel': True
-        'CSV': False
+        'Excel': True,
+        'CSV': False,
         'Dataframe as pickle': True
     }
 
